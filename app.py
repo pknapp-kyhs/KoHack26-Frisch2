4cba8497d6ce568acf7be7b2370ba0cc140674e8
cp3453 <cp3453@columbia.edu>
2026-03-22 12:31:58 -0400

    Created login.htm and added session to main.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
