0f1ee348d50d6ccd5df3f6dcf81162b314c105ad
cp3453 <cp3453@columbia.edu>
2026-03-22 12:34:31 -0400

    Stuff

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# TODO Implement this
 @app.route('/login/')
 def login():
     return render_template('login.html')
+
+# TODO Also implement this
+@app.route("/sign-up")
+def signup():
+    return render_template('sign-up.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
